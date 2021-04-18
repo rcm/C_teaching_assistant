446a60ab18dd253acd637272e42d6d3410aa9cc5
Rui Mendes <azuki@di.uminho.pt>
2021-04-18 19:18:51 +0100

    Continuing refactoring. Split example report into other filename.

--- consult_files.py
+++ consult_files.py
@@ -1,16 +1,13 @@
 import shutil
 import os
 import subprocess
-import re
 import tempfile
 import json
 import glob
 from collections import defaultdict
 import weakref
-import tabfun
-import statistics
+import re
 from utilities import *
-
 
 class KeepRefs(object):
     __refs__ = defaultdict(list)
@@ -189,6 +186,12 @@ class Table:
         tab.rows = self.rows + other.rows
         return tab
 
+    def add_to_environ(self, dic):
+        globals().update(dic)
+
+    def get_environ(self):
+        return globals()
+
     def add_row(self, **args):
         row_headers = [K for K in args.keys()]
         if self.headers is None:
@@ -197,10 +200,10 @@ class Table:
         self.rows.append(args)
 
     def select(self, condition):
-        self.rows = [row for row in self.rows if eval(re.sub(r'\w+', substitute(row), condition))]
+        self.rows = [row for row in self.rows if eval(re.sub(r'\w+', substitute(row), condition), self.get_environ())]
 
     def transform(self, headers):
-        self.rows = [{K : row[K] if K in row else eval(re.sub(r'\w+', substitute(row), K)) for K in headers}
+        self.rows = [{K : row[K] if K in row else eval(re.sub(r'\w+', substitute(row), K), self.get_environ()) for K in headers}
             for row in self.rows]
         self.headers = headers
 
@@ -210,7 +213,7 @@ class Table:
         self.headers = headers
 
     def sort(self, headers):
-        self.rows = sorted(self.rows, key = lambda R : [eval(re.sub(r'\w+', substitute(R), K)) for K in headers])
+        self.rows = sorted(self.rows, key = lambda R : [eval(re.sub(r'\w+', substitute(R), K), self.get_environ()) for K in headers])
 
     def color_by(self, colors):
         self.colors = {self.headers.index(K): V for K, V in colors.items()}
@@ -218,7 +221,7 @@ class Table:
     def group_by(self, headers, aggreg):
         tbl = {}
         for row in self.rows:
-            grp = tuple(eval(re.sub(r'\w+', substitute(row), K)) for K in headers)
+            grp = tuple(eval(re.sub(r'\w+', substitute(row), K), self.get_environ()) for K in headers)
             if grp not in tbl:
                 tbl[grp] = {}
             for K, V in row.items():
@@ -227,7 +230,7 @@ class Table:
         for grp, grp_val in tbl.items():
             row = {K: V for K, V in zip(headers, grp)}
             for agg in aggreg:
-                row[agg] = eval(re.sub(r'\w+', substitute(grp_val), agg))
+                row[agg] = eval(re.sub(r'\w+', substitute(grp_val), agg), self.get_environ())
             res.add_row(**row)
         self.headers = res.headers
         self.rows = res.rows
@@ -250,51 +253,3 @@ class Table:
 PythonLanguage = ProgrammingLanguage("Python",[PythonFile])
 CLanguage = ProgrammingLanguage("C", [HFile, CFile])
 
-if False:
-    T = None
-    for folder in glob.glob("/home/rui/repos/LCC*") + glob.glob("/home/rui/repos/MIEI*"):
-        if T is None:
-             T = CodeFolder(folder).create_function_table()
-        else:
-            tab = CodeFolder(folder).create_function_table()
-            if tab:
-                T = T + tab
-    T.to_json("cached_results.json")
-else:
-    T = Table()
-    T.from_json("cached_results.json")
-
-
-def col_CC(x):
-    if x < 10: return tabfun.GREEN
-    if x < 20: return tabfun.ORANGE
-    if x < 30: return tabfun.YELLOW
-    return tabfun.RED
-def col_MI(x):
-    if x > 80: return tabfun.GREEN
-    if x > 60: return tabfun.ORANGE
-    if x > 40: return tabfun.YELLOW
-    return tabfun.RED
-
-def col_BP(x):
-    if x < 0.05: return tabfun.GREEN
-    if x < 0.01: return tabfun.ORANGE
-    if x < 0.1: return tabfun.YELLOW
-    return tabfun.RED
-
-def iqm(values, pos = 2):
-    q = min(values), *statistics.quantiles(values), max(values)
-    return statistics.mean([x for x in values if q[pos - 1] <= x <= q[pos + 1]])
-
-T.transform('folder.replace("/home/rui/repos/","") name re.sub(r"^/tmp/tmp.*?/copy/","",filename) cyclomatic_complexity loc maintainability_index halstead_bugprop'.split())
-T.rename("folder name filename CC loc MI BP".split())
-T.sort("folder MI -CC -loc filename name".split())
-#T.select("MI < 80 or CC > 10 or BP > 0.05")
-T.transform("folder name filename loc CC MI BP".split())
-#T.color_by({'CC' : col_CC, 'MI' : col_MI, 'BP' : col_BP})
-
-T.group_by("folder".split(), ["len(CC)", "round(100 * sum(x < 10 for x in CC)/len(CC),2)", "max(CC)",  "iqm(CC,3)", "round(100*sum(x > 80 for x in MI)/len(MI),2)", "min(MI)", "iqm(MI,1)"])
-T.rename(["folder", "tam", "good_CC", "wrst_CC", "iqm_CC", "good_MI", "wrst_MI", "iqm_MI"])
-T.color_by({'wrst_CC' : scale_lower(10,80), 'wrst_MI' : scale_upper(0, 80)})
-#T.select('folder == "MIEIPL2G01"')
-print(T.tabulate())
---
+++ lab2021.py
@@ -0,0 +1,56 @@
+import tabfun
+import re
+import statistics
+from consult_files import *
+
+if True:
+    T = None
+    for folder in glob.glob("/home/rui/repos/LCC*") + glob.glob("/home/rui/repos/MIEI*"):
+        if T is None:
+            T = CodeFolder(folder).create_function_table()
+        else:
+            tab = CodeFolder(folder).create_function_table()
+            if tab:
+                T = T + tab
+    T.to_json("cached_results.json")
+else:
+    T = Table()
+    T.from_json("cached_results.json")
+
+
+def col_CC(x):
+    if x < 10: return tabfun.GREEN
+    if x < 20: return tabfun.ORANGE
+    if x < 30: return tabfun.YELLOW
+    return tabfun.RED
+def col_MI(x):
+    if x > 80: return tabfun.GREEN
+    if x > 60: return tabfun.ORANGE
+    if x > 40: return tabfun.YELLOW
+    return tabfun.RED
+
+def col_BP(x):
+    if x < 0.05: return tabfun.GREEN
+    if x < 0.01: return tabfun.ORANGE
+    if x < 0.1: return tabfun.YELLOW
+    return tabfun.RED
+
+def iqm(values, pos = 2):
+    q = min(values), *statistics.quantiles(values), max(values)
+    return statistics.mean([x for x in values if q[pos - 1] <= x <= q[pos + 1]])
+
+T.add_to_environ(globals())
+T.transform('folder.replace("/home/rui/repos/","") name re.sub(r"^/tmp/tmp.*?/copy/","",filename) cyclomatic_complexity loc maintainability_index halstead_bugprop'.split())
+T.rename("folder name filename CC loc MI BP".split())
+T.sort("folder MI -CC -loc filename name".split())
+#T.select("MI < 80 or CC > 10 or BP > 0.05")
+T.transform("folder name filename loc CC MI BP".split())
+#T.color_by({'CC' : col_CC, 'MI' : col_MI, 'BP' : col_BP})
+
+T.group_by("folder".split(), ["len(CC)", "round(100 * sum(x < 10 for x in CC)/len(CC),2)", "max(CC)",  "iqm(CC,3)", "round(100*sum(x > 80 for x in MI)/len(MI),2)", "min(MI)", "iqm(MI,1)"])
+T.rename(["folder", "tam", "good_CC", "wrst_CC", "iqm_CC", "good_MI", "wrst_MI", "iqm_MI"])
+T.transform(["folder", "wrst_CC", "wrst_MI", "round(100 * scale_lower(10,80)(wrst_CC), 2)", "round(100 * scale_upper(10,80)(wrst_MI), 2)", "round(50 * (scale_lower(10,80)(wrst_CC) + scale_upper(10,80)(wrst_MI)), 2)"])
+T.rename(["folder", "CC", "MI", "grdCC", "grdMI", "nota"])
+T.color_by({'CC' : scale_lower(10,80), 'MI' : scale_upper(0, 80)})
+#T.select('folder == "MIEIPL2G01"')
+print(T.tabulate())
